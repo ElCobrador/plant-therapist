15f88baea802176e7c1ac56ae24ddf5f3368f347
Philippe Audet <philippe.audet@outlook.com>
2023-04-04 22:26:27 -0400

    Added jest

---
+++ src/tests/userService.test.ts
@@ -0,0 +1,9 @@
+import { User } from '@/interfaces/User.interface';
+import { describe, expect, test } from '@jest/globals'
+
+describe('user.service', () => {
+  test('get users should return users', () => {
+    const expectedUsers: Array<User> = [];
+    expect(5).toBe(5);
+  })
+})
