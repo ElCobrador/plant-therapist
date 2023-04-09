6ed8c99e046067e0664529ca74b5530f216c7906
Philippe Audet <philippe.audet@outlook.com>
2023-04-09 00:36:26 -0400

    removed the authentication from the docker mongo db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,16 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config/index';
+import { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } from '@config/index';
+import { ConnectOptions } from 'mongoose';
 
 export const dbConnection = {
   url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
+    poolSize: 10,
+    authSource: "admin",
+    user: DB_USERNAME,
+    pass: DB_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
+    useCreateIndex: true,
+    useFindAndModify: false
+  } as ConnectOptions,
 };
