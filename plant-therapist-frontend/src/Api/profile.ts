eeecc3efff8300c72ec762400ccab315df759d86
Philippe Audet <philippe.audet@outlook.com>
2023-06-10 20:55:29 -0400

    Fix git error on profile ?

--- plant-therapist-frontend/src/Api/profile.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const endpoint = "http://localhost:3000"
-
-async function fetchUser(userId: string): User {
-  try {
-    const response = await axios.get()
-  }
-}
--- plant-therapist-frontend/src/api/profile.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const endpoint = "http://localhost:3000"
-
-async function fetchUser(userId: string) {
-  throw new Error('Not implemented');
-}
